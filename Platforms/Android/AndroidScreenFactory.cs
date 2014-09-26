b0f387278cdb06210ff1be9be4183185be1f30c6
Glory Lo <gloryl604@gmail.com>
2014-09-26 09:50:20 -0700

    added android screen

---
+++ Platforms/Android/AndroidScreenFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joyride.Platforms.Android
+{
+    public class AndroidScreenFactory : ScreenFactory
+    {
+        public override T CreateScreen<T>()
+        {
+            if (!typeof(T).IsSubclassOf(typeof(AndroidScreen)))
+                throw new Exception("Unable to create screen of type:  " + typeof(T));
+            return new T();
+        }
+    }
+}
