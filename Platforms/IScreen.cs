b7d0c3827745d4cec3598625008a183209876659
Glory Lo <gloryl604@gmail.com>
2014-10-01 12:59:02 -0700

    created pof.mobleapps project and moved iphone app into it

--- Platforms/IScreen.cs
+++ Platforms/IScreen.cs
@@ -4,6 +4,7 @@
     // not solidified yet.  to be used later.
     public interface IScreen : ITouchGestures, IWebViewActions, IFormActions
     {
+        // would like to get rid of this time out..
         bool IsOnScreen(int timeOutSecs = Component.DefaultWaitSeconds);
         string Name { get; }
     }
