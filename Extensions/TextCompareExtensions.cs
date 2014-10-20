c3e6fdb572ab0765fa5809f6385ade7b90eb6975
Glory Lo <gloryl604@gmail.com>
2014-10-20 16:20:59 -0700

    added android label step

---
+++ Extensions/TextCompareExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace Joyride.Extensions
+{
+    public static class TextCompareExtensions
+    {
+        public static bool CompareWith(this string yourString, string compareWithString, TextCompare compareType)
+        {
+            switch (compareType)
+            {
+                case TextCompare.Equals:
+                    return yourString == compareWithString;
+
+                case TextCompare.NotEqual:
+                    return yourString != compareWithString;
+
+                case TextCompare.StartsWith:
+                    return yourString.StartsWith(compareWithString);
+
+                case TextCompare.EndsWith:
+                    return yourString.EndsWith(compareWithString);
+
+                case TextCompare.Containing:
+                    return yourString.Contains(compareWithString);
+
+                case TextCompare.Matching:
+                    var match = Regex.Match(yourString, compareWithString);
+                    return match.Success;
+
+                default:
+                    throw new Exception("Unknown compare type:  " + compareType);
+
+            }
+
+        }
+
+
+    }
+}
--- Platforms/Android/AndroidScreen.cs
+++ Platforms/Android/AndroidScreen.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Collections.ObjectModel;
 using OpenQA.Selenium;
 
 namespace Joyride.Platforms.Android
@@ -22,5 +24,73 @@ namespace Joyride.Platforms.Android
             //Driver.Navigate().Back();
             return this;
         }
+
+        public virtual bool HasLabel(string label, TextCompare compareType)
+        {
+            return HasText(label, compareType) || HasContentDesc(label, compareType);
+        }
+
+        protected bool HasContentDesc(string label, TextCompare compareType)
+        {
+            ReadOnlyCollection<IWebElement> texts = null;
+            switch (compareType)
+            {
+                case TextCompare.Equals:
+                    texts = Driver.FindElementsByAndroidUIAutomator(@"new UiSelector().description(""" + label + @""")");
+                    break;
+
+                case TextCompare.StartsWith:
+                    texts =
+                        Driver.FindElementsByAndroidUIAutomator(@"new UiSelector().descriptionStartsWith(""" + label + @""")");
+                    break;
+
+                case TextCompare.Matching:
+                    texts = Driver.FindElementsByAndroidUIAutomator(@"new UiSelector().descriptionMatches(""" + label + @""")");
+                    break;
+
+                case TextCompare.Containing:
+                    texts = Driver.FindElementsByAndroidUIAutomator(@"new UiSelector().descriptionContains(""" + label + @""")");
+                    break;
+                default:
+                    throw new NotImplementedException("Not implemented compare type: " + compareType);
+            }
+
+            if (texts != null && texts.Count != 0)
+                return true;
+
+            return false;
+        }
+
+        protected bool HasText(string label, TextCompare compareType)
+        {
+            ReadOnlyCollection<IWebElement> texts = null;
+            switch (compareType)
+            {
+                case TextCompare.Equals:
+                    texts = Driver.FindElementsByAndroidUIAutomator(@"new UiSelector().text(""" + label + @""")");
+                    break;
+
+                case TextCompare.StartsWith:
+                    texts =
+                        Driver.FindElementsByAndroidUIAutomator(@"new UiSelector().textStartsWith(""" + label + @""")");
+                    break;
+
+                case TextCompare.Matching:
+                    texts = Driver.FindElementsByAndroidUIAutomator(@"new UiSelector().textMatches(""" + label + @""")");
+                    break;
+
+                case TextCompare.Containing:
+                    texts = Driver.FindElementsByAndroidUIAutomator(@"new UiSelector().textContains(""" + label + @""")");
+                    break;
+                default:
+                    throw new NotImplementedException("Not implemented compare type: " + compareType);
+            }
+
+            if (texts != null && texts.Count != 0)
+                return true;
+
+            return false;
+        }
+
     }
 }
