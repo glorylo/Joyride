5c07f57dcafbe00b22d9af80eba9a72e1ea99ee9
Glory Lo <gloryl604@gmail.com>
2015-03-19 13:48:27 -0700

    added property condition to check if they are mandatory

--- Interfaces/IEntryEnumerable.cs
+++ Interfaces/IEntryEnumerable.cs
@@ -2,6 +2,9 @@
 
 namespace Joyride.Interfaces
 {
+    /// <summary>
+    /// Allows screens to iterate through entries within a collection
+    /// </summary>
     public interface IEntryEnumerable : IEnumerable<IDictionary<string, object>>
     {
     }
