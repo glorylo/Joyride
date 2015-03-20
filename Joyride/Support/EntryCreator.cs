5b7701d4a1b43e430dc724cc48f9a5bd0c35c615
Glory Lo <gloryl604@gmail.com>
2015-03-20 12:19:43 -0700

    entry creator now takes in collection name

--- Joyride/Support/EntryCreator.cs
+++ Joyride/Support/EntryCreator.cs
@@ -8,29 +8,36 @@ namespace Joyride.Support
 {
     public delegate object ProcessValue(string key, string value);
 
+    /// <summary>
+    /// Utility class to create entries off from a collection.  Provide the component, the collection and a element map (property, relative xpath pairs)
+    /// Optionally can take a delegate to process text values such as extracting something from a known format.  
+    /// </summary>
     public class EntryCreator
     {
         private readonly Component _component;        
         private readonly IDictionary<string, string> _elementMap;
+        private readonly string _collectionName;
         private readonly ProcessValue _processValue;
 
-        public EntryCreator(Component component, IDictionary<string, string> elementMap)
+        public EntryCreator(Component component, string collectionName, IDictionary<string, string> elementMap)
         {
             _component = component;
             _elementMap = elementMap;
+            _collectionName = collectionName;
             _processValue = (key, text) => text;
         }
 
-        public EntryCreator(Component component, IDictionary<string, string> elementMap, ProcessValue processText)
+        public EntryCreator(Component component, string collectionName, IDictionary<string, string> elementMap, ProcessValue processText)
         {
             _component = component;
             _elementMap = elementMap;
+            _collectionName = collectionName;
             _processValue = processText;
         }
 
-        private IDictionary<string, object> AddProperty(IDictionary<string, object> dictonary, string collectionName, int index, string xpath, string key)
+        private IDictionary<string, object> AddProperty(IDictionary<string, object> dictonary, int index, string xpath, string key)
         {
-            var text = _component.GetTextFromElementWithinCollection(collectionName, index, xpath);
+            var text = _component.GetTextFromElementWithinCollection(_collectionName, index, xpath);
             if (text != null)
             {
                 Trace.WriteLine("Found property, " + key + ", with value: " + text);
@@ -44,10 +51,10 @@ namespace Joyride.Support
             return dictonary;
         }
 
-        public IDictionary<string, object> GetNextEntry(string collectionName, int index)
+        public IDictionary<string, object> GetNextEntry(int index)
         {
             var dictionary = new ExpandoObject() as IDictionary<string, object>;
-            return _elementMap.Aggregate(dictionary, (current, e) => AddProperty(current, collectionName, index, e.Value, e.Key));
+            return _elementMap.Aggregate(dictionary, (current, e) => AddProperty(current, index, e.Value, e.Key));
         }
     }
 }
