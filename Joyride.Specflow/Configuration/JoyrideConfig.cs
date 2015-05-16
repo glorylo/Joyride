76ffc24841745fd92b4b84f495d6dd37d4ab5aec
Glory Lo <gloryl604@gmail.com>
2015-05-15 21:14:01 -0700

    add experimental unused types

--- Joyride.Specflow/Configuration/JoyrideConfig.cs
+++ Joyride.Specflow/Configuration/JoyrideConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.Configuration;
-using System.Runtime.InteropServices.ComTypes;
+﻿using System.Configuration;
 
 
 namespace Joyride.Specflow.Configuration
@@ -13,20 +10,16 @@ namespace Joyride.Specflow.Configuration
         public static JoyrideConfig Settings { get { return _settings;  }}
 
         [ConfigurationProperty("capabilities", IsRequired = true)]
-        public CapabilityElement Capabilities { get { return (CapabilityElement)base["capabilities"]; } }
+        public GlobalCapabilityElement Capabilities { get { return (GlobalCapabilityElement) base["capabilities"]; } }
         
-
         [ConfigurationProperty("android")]
         public AndroidElement Android { get { return (AndroidElement) base["android"]; } }
-
         
         [ConfigurationProperty("ios")]
-        public IosElement Ios { get { return (IosElement) base["ios"]; } }
-        
+        public IosElement Ios { get { return (IosElement) base["ios"]; } }        
 
         [ConfigurationProperty("endpoints", IsRequired = true)]
         public EndPointElement Endpoints { get { return (EndPointElement) base["endpoints"]; } } 
-
 
 
     }
@@ -51,9 +44,50 @@ namespace Joyride.Specflow.Configuration
         public NameValueConfigurationCollection Settings { get { return (NameValueConfigurationCollection)base[""]; } }
     }
 
-    public class CapabilityElement : ConfigurationElement
+    public class GlobalCapabilityElement : ConfigurationElement
     {
         [ConfigurationProperty("", IsDefaultCollection = true)]
         public NameValueConfigurationCollection Settings { get { return (NameValueConfigurationCollection)base[""]; } }
     }
+
+    /*
+    public class CapabilityElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsRequired = true)]
+        public string Name
+        {
+            get { return (string) this["name"]; }
+            set { this["name"] = value; }
+        }
+
+        [ConfigurationProperty("value", IsRequired = true)]
+        public object Value
+        {
+            get { return this["value"]; }
+            set { this["value"] = value; }
+        }
+
+        [ConfigurationProperty("type", IsRequired = true)]
+        public string Type
+        {
+            get { return (string) this["type"]; }
+            set { this["type"] = value; }
+        }
+
+        public class CapabilityElementCollection : ConfigurationElementCollection
+        {
+            protected override ConfigurationElement CreateNewElement()
+            {
+                return new CapabilityElement();
+            }
+
+            protected override object GetElementKey(ConfigurationElement element)
+            {
+                return ((CapabilityElement) element).Name;
+            }
+        }
+     
+    }
+      
+     */
 }
