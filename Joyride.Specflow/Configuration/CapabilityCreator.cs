bb8f5087d07ba4e71e158c21b122a2cfa995462f
Glory Lo <gloryl604@gmail.com>
2015-05-15 19:36:55 -0700

    added capability creator

---
+++ Joyride.Specflow/Configuration/CapabilityCreator.cs
@@ -0,0 +1,57 @@
+﻿using System.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace Joyride.Specflow.Configuration
+{
+    public class CapabilityCreator
+    {
+        public const string Server = "local";
+        public static Platform Platform { get; set; }
+        public DesiredCapabilities Capabilities { get; set; }
+        private readonly JoyrideConfig _config = JoyrideConfig.Settings;
+        private readonly NameValueConfigurationCollection _globalCapabilities = JoyrideConfig.Settings.Capabilities.Settings;
+
+
+        public CapabilityCreator(Platform platform)
+        {         
+            Platform = platform;
+            Capabilities = new DesiredCapabilities();
+        }
+
+        public DesiredCapabilities Create()
+        {
+            LoadCapabilities(_globalCapabilities);
+            
+            if (Platform == Platform.Android)
+            {
+                LoadCapabilities(_config.Android.Settings);    
+            }
+            else // if (Platform == Platform.Ios)
+            {
+                
+            }
+
+            return Capabilities;
+        }
+
+        private void LoadCapabilities(NameValueConfigurationCollection settings)
+        {
+            foreach (var key in settings.AllKeys)
+            {                
+                Capabilities.SetCapability(settings[key].Name, settings[key].Value);
+            }
+
+        }
+        private string GetServerAddress(JoyrideConfig config, string key)
+        {
+            var endpoints = config.Endpoints.Settings;
+            var server = endpoints[key];
+
+            if (server == null)
+                throw new NotFoundException("Unable to find server with key: " + key);
+
+            return server.Value;
+        }
+    }
+}
---
+++ Joyride.Specflow/Configuration/JoyrideConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace Joyride.Specflow.Configuration
+{
+    public class JoyrideConfig : ConfigurationSection
+    {
+        private static readonly JoyrideConfig _settings = ConfigurationManager.GetSection("joyride") as JoyrideConfig;
+
+        public static JoyrideConfig Settings { get { return _settings;  }}
+
+        [ConfigurationProperty("capabilities", IsRequired = true)]
+        public CapabilityElement Capabilities { get { return (CapabilityElement)base["capabilities"]; } }
+        
+
+        [ConfigurationProperty("android")]
+        public AndroidElement Android { get { return (AndroidElement) base["android"]; } }
+
+        
+        [ConfigurationProperty("ios")]
+        public IosElement Ios { get { return (IosElement) base["ios"]; } }
+        
+
+        [ConfigurationProperty("endpoints", IsRequired = true)]
+        public EndPointElement Endpoints { get { return (EndPointElement) base["endpoints"]; } } 
+
+
+
+    }
+
+    public class EndPointElement : ConfigurationElement
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        public NameValueConfigurationCollection Settings { get {  return (NameValueConfigurationCollection) base[""]; }}
+
+    }
+
+    public class IosElement : ConfigurationElement
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        public NameValueConfigurationCollection Settings { get { return (NameValueConfigurationCollection)base[""]; } }
+        
+    }
+
+    public class AndroidElement : ConfigurationElement
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        public NameValueConfigurationCollection Settings { get { return (NameValueConfigurationCollection)base[""]; } }
+    }
+
+    public class CapabilityElement : ConfigurationElement
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        public NameValueConfigurationCollection Settings { get { return (NameValueConfigurationCollection)base[""]; } }
+    }
+}
