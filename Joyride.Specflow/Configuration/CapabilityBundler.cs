e7cae82e7c536ecea4e12b39a911780314f61972
Glory Lo <gloryl604@gmail.com>
2015-05-19 15:37:25 -0700

    added capability bundler

---
+++ Joyride.Specflow/Configuration/CapabilityBundler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandyConfig.Configuration;
+using OpenQA.Selenium.Remote;
+
+namespace Joyride.Specflow.Configuration
+{
+    public class CapabilityBundler
+    {
+        private DesiredCapabilities _capabilities;
+        private readonly Platform _platform;
+        public CapabilityBundler(Platform platform)
+        {
+            _platform = platform;
+        }
+
+        public DesiredCapabilities Bundle(string targetName)
+        {
+            IDictionary<string, object> configs = new Dictionary<string, object>();
+            var configBundler = new ConfigBundler(configs);
+            configBundler.Bundle(CapabilitiesElement.Settings);
+            if (_platform == Platform.Android)
+                configBundler.Bundle(AndroidElement.Settings);
+            else
+                configBundler.Bundle(IosElement.Settings);
+            
+            configs = configBundler.GetConfigs();
+            _capabilities = new DesiredCapabilities(configs as Dictionary<string, object>);
+            return _capabilities;
+        }
+
+
+
+    }
+}
---
+++ Joyride.Specflow/Configuration/JoyrideSectionHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Configuration;
+using HandyConfig.Configuration;
+
+
+namespace Joyride.Specflow.Configuration
+{
+    public class JoyrideSectionHandler : ConfigurationSection
+    {
+        public static JoyrideSectionHandler Settings { get { return ConfigurationManager.GetSection("joyride") as JoyrideSectionHandler;  }}
+
+        [ConfigurationProperty("capabilities", IsRequired = true)]
+        public CapabilitiesElement Capabilities { get { return (CapabilitiesElement) base["capabilities"]; } }
+        
+        [ConfigurationProperty("android")]
+        public AndroidElement Android { get { return (AndroidElement) base["android"]; } }
+        
+        [ConfigurationProperty("ios")]
+        public IosElement Ios { get { return (IosElement) base["ios"]; } }        
+
+        [ConfigurationProperty("servers", IsRequired = true)]
+        public ServersElement Servers { get { return (ServersElement) base["servers"]; } } 
+
+
+    }
+
+    public class DesiredCapabilityElement : ConfigurationElement
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        [ConfigurationCollection(typeof(NameValueTypeElementCollection), AddItemName = "add")]
+        public NameValueTypeElementCollection NameValueTypes
+        {
+            get { return (NameValueTypeElementCollection) this[""]; }
+            set { this[""] = value; }
+        }        
+    }
+
+    public class ServersElement : DesiredCapabilityElement
+    {
+        public static NameValueTypeElementCollection Settings = JoyrideSectionHandler.Settings.Servers.NameValueTypes;
+
+ 
+    }
+
+    public class IosElement : DesiredCapabilityElement
+    {
+        public static NameValueTypeElementCollection Settings = JoyrideSectionHandler.Settings.Ios.NameValueTypes;
+        
+    }
+
+    public class AndroidElement : DesiredCapabilityElement
+    {
+        public static NameValueTypeElementCollection Settings = JoyrideSectionHandler.Settings.Android.NameValueTypes;
+
+    }
+
+    public class CapabilitiesElement : DesiredCapabilityElement
+    {
+        public static NameValueTypeElementCollection Settings = JoyrideSectionHandler.Settings.Capabilities.NameValueTypes;
+
+    }
+
+
+}
