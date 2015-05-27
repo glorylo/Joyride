83148d75b203db634533a296009cd12fcf904d3f
Glory Lo <gloryl604@gmail.com>
2015-05-27 10:43:45 -0700

    added screenfactory into app

--- Joyride/Platforms/Android/AndroidMobileApplication.cs
+++ Joyride/Platforms/Android/AndroidMobileApplication.cs
@@ -3,5 +3,6 @@ namespace Joyride.Platforms.Android
 {
     abstract public class AndroidMobileApplication : MobileApplication
     {
+        protected static readonly ScreenFactory ScreenFactory = new AndroidScreenFactory();
     }
 }
--- Joyride/Platforms/Ios/IosMobileApplication.cs
+++ Joyride/Platforms/Ios/IosMobileApplication.cs
@@ -3,5 +3,6 @@ namespace Joyride.Platforms.Ios
 {
     abstract public class IosMobileApplication : MobileApplication
     {
+        protected static readonly ScreenFactory ScreenFactory = new IosScreenFactory();
     }
 }
