45014928bd706a3a4bf4e1f2e494db91a9080106
Glory Lo <gloryl604@gmail.com>
2015-06-08 13:42:20 -0700

    reorganized methods in screen

--- Joyride/Platforms/ITouchGesture.cs
+++ Joyride/Platforms/ITouchGesture.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using OpenQA.Selenium;
-
+﻿
 namespace Joyride.Platforms
 {
 
@@ -10,12 +8,11 @@ namespace Joyride.Platforms
         Screen DoubleTap(string elementName);
         Screen TapAndHold(string elementName, int seconds);
         Screen TapInCollection(string collectionName, int oridinal = 1, bool last = false, bool precise = false);
-        Screen TapInCollection(string collectionName, Predicate<IWebElement> predicate);
         Screen PinchToZoom(Direction direction, double scale = 1.0);
         Screen Scroll(Direction direction, double scale=1.0, long durationMilliSecs = 500);
         Screen Scroll(string elementName, Direction direction, double scale=1.0, long durationMilliSecs = 500);
         Screen Swipe(Direction direction, double scale=1.0, long durationMilliSecs = 500);
         Screen Swipe(string elementName, Direction direction, double scale=1.0, long durationMilliSecs = 500);
-        Screen ScrollUntil(string elementName, Direction direction, double scale=1.0, long durationMilliSecs = 500, int maxRetries = 30);
+        Screen ScrollUntil(string elementName, Direction direction, double scale=1.0, long durationMilliSecs = 500, int maxRetries = 60);
     }
 }
--- Joyride/Platforms/Screen.cs
+++ Joyride/Platforms/Screen.cs
@@ -108,11 +108,7 @@ namespace Joyride.Platforms
 
         #endregion
 
-        public virtual Screen Rotate(ScreenOrientation orientation)
-        {
-            Driver.Orientation = orientation;
-            return this;
-        }
+        #region Gestures
 
         internal protected void TapInWebview(string elementName)
         {
@@ -197,64 +193,6 @@ namespace Joyride.Platforms
             return this;
         }
 
-      
-        public virtual Screen EnterText(string elementName, string text)
-        {
-            var element = FindElement(elementName);
-
-            if (element == null)
-                throw new NoSuchElementException("Cannot find element:  " + elementName);
-
-            element.Clear();
-            element.SendKeys(text);
-            return this;
-        }
-
-        public virtual Screen SetCheckbox(string elementName, bool enabled = true)
-        {
-            var element = FindElement(elementName);
-
-            if (element == null)
-                throw new NoSuchElementException("Cannot find element:  " + elementName);
-
-            var selected = element.Selected;
-            if ((enabled && !selected) || (!enabled && selected))
-                element.Click();
-
-            return this;
-        }
-
-        // only works in webview currently
-        public virtual Screen SelectOption(string elementName, string value)
-        {
-            Driver.DoActionInWebView(() =>
-            {
-                var selectElement = FindElement(elementName);
-                if (selectElement == null)
-                    throw new NoSuchElementException("Cannot find element:  " + elementName);
-
-                var selector = new SelectElement(selectElement);
-                selector.SelectByText(value);
-            });
-            return this;
-        }
-
-        // only works in webview currently
-        public virtual string GetSelectedOption(string elementName)
-        {
-            string selected = null;
-            Driver.DoActionInWebView(() =>
-            {
-                var selectElement = FindElement(elementName);
-                if (selectElement == null)
-                    throw new NoSuchElementException("Cannot find element:  " + elementName);
-
-                var selector = new SelectElement(selectElement);
-                selected = selector.SelectedOption.Text;
-            });
-            return selected;
-        }
-
         public virtual Screen Scroll(Direction direction, double scale=1.0, long durationMilliSecs = 500)
         {
             Driver.Scroll(direction, scale, durationMilliSecs);
@@ -304,7 +242,79 @@ namespace Joyride.Platforms
             }
             throw new NoSuchElementException("Unable to find visible element: " + elementName);
         }
+        #endregion 
 
+        #region Form Actions
+
+        public virtual Screen EnterText(string elementName, string text)
+        {
+            var element = FindElement(elementName);
+
+            if (element == null)
+                throw new NoSuchElementException("Cannot find element:  " + elementName);
+
+            element.Clear();
+            element.SendKeys(text);
+            return this;
+        }
+
+        public virtual Screen SetCheckbox(string elementName, bool enabled = true)
+        {
+            var element = FindElement(elementName);
+
+            if (element == null)
+                throw new NoSuchElementException("Cannot find element:  " + elementName);
+
+            var selected = element.Selected;
+            if ((enabled && !selected) || (!enabled && selected))
+                element.Click();
+
+            return this;
+        }
+
+        #endregion
+
+        #region WebView
+
+        // only works in webview currently
+        public virtual Screen SelectOption(string elementName, string value)
+        {
+            Driver.DoActionInWebView(() =>
+            {
+                var selectElement = FindElement(elementName);
+                if (selectElement == null)
+                    throw new NoSuchElementException("Cannot find element:  " + elementName);
+
+                var selector = new SelectElement(selectElement);
+                selector.SelectByText(value);
+            });
+            return this;
+        }
+
+        // only works in webview currently
+        public virtual string GetSelectedOption(string elementName)
+        {
+            string selected = null;
+            Driver.DoActionInWebView(() =>
+            {
+                var selectElement = FindElement(elementName);
+                if (selectElement == null)
+                    throw new NoSuchElementException("Cannot find element:  " + elementName);
+
+                var selector = new SelectElement(selectElement);
+                selected = selector.SelectedOption.Text;
+            });
+            return selected;
+        }
+
+        #endregion
+
+        public virtual Screen Rotate(ScreenOrientation orientation)
+        {
+            Driver.Orientation = orientation;
+            return this;
+        }
+        
         public string GetSourceWebView()
         {
             string source = null;
