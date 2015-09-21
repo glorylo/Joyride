43feff82a996332cf70e060b8459d10caa352b86
Glory Lo <gloryl604@gmail.com>
2015-09-21 14:28:13 -0700

    added page indicator step for ios

--- Joyride.Specflow/Steps/DeprecatedSteps.cs
+++ Joyride.Specflow/Steps/DeprecatedSteps.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using Joyride.Extensions;
 using Joyride.Platforms;
+using Joyride.Platforms.Ios;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 
@@ -30,7 +31,18 @@ namespace Joyride.Specflow.Steps
                  "Unexpected text compare with '" + actualLabel + "' is " + compareType + " '" + text + "'");
         }
 
+        [Then(@"I should see the ""(\d+)"" in collection ""([^""]*)"" with (name|value) (equals|starts with|containing|matching) ""([^""]*)""")]
+        public void ThenIShouldSeeElementAttributeValueInCollectionCompareWithText(int index, string collectionName, string nameOrValue, string compareType, string text)
+        {
+            string attributeValue = null;
+            Context.MobileApp.Do<IosScreen>(s => attributeValue = s.GetElementAttribute(collectionName, index, nameOrValue));
 
+            if (attributeValue == null)
+                Assert.Fail("Unable to find attribute '" + nameOrValue + "' for '" + index + "' item in " + collectionName);
+
+            Assert.That(attributeValue.CompareWith(text, compareType.ToCompareType()), Is.True,
+                "Unexpected text compare with '" + attributeValue + "' is not + " + compareType + " '" + text + "'");
+        }
 
 
     }
